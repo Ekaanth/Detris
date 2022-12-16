1474062fd5580b3fe6cffcf2e05baaeaa3156328
Abhishek <akanth1994@gmail.com>
2022-12-17 00:39:51 +0530

    Initial game commit

---
+++ detris.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("detris");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.clientWidth, canvas.height);
